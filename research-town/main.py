7e849d549cf24d083cfc58acf0dcd3d97ddd3291
ft2023 <129666640+ft2023@users.noreply.github.com>
2024-05-02 23:51:28 +0800

    Add files via upload

---
+++ research-town/main.py
@@ -0,0 +1,14 @@
+from  function_class import *
+
+single_agent=SingleAgent()
+# print(single_agent.get_profile("Jiaxuan You"))
+# print(single_agent.get_recent_paper_info(10,"Machine Learning"))
+# domain="Machine Learning"
+# profile=single_agent.get_profile("Jiaxuan You")
+# papers=single_agent.get_recent_paper_info(10,"Machine Learning")
+#
+# ideas=single_agent.generate_ideas(profile,papers,domain)
+# print(ideas)
+
+multi_agent=MultiAgent()
+print(multi_agent.get_relation_graph("Jiaxuan You",20))
