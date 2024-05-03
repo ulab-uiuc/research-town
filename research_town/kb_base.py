11c667cf0dd3c88206c9c984a4ab99514a017b3a
Haofei Yu <1125027232@qq.com>
2024-05-03 02:07:01 -0400

    support empty class structure (#3)
    * just create a structure
    
    * pass all tests

---
+++ research_town/kb_base.py
@@ -0,0 +1,14 @@
+from typing import List, Tuple, Dict
+
+class BaseKnowledgeBase(object):
+    def __init__(self) -> None:
+        self.data: Dict[str, str] = {}
+
+    def update_kb(self, data: Dict[str, str]) -> None:
+        self.data.update(data)
+    
+    def add_data(self, data: Dict[str, str]) -> None:
+        self.data.update(data)
+    
+    def get_data(self, key: str) -> str:
+        return self.data[key]
