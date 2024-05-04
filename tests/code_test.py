cb1003ab87ffb3086cc76ff663052ed5c909e216
ft2023 <129666640+ft2023@users.noreply.github.com>
2024-05-04 21:29:44 +0800

    Add files via upload

---
+++ tests/code_test.py
@@ -0,0 +1,11 @@
+from agent_base import *
+from kb_base import *
+base_agent=BaseResearchAgent('Jiaxuan You')
+# print(base_agent.profile)
+
+kb=BaseKnowledgeBase()
+# print(kb.get_data(10,"Machine Learning"))
+data=kb.get_data(10,"Machine Learning")
+paper=base_agent.read_paper(data,"Machine Learning")
+idea=base_agent.generate_idea(data,"Machine Learning")
+relations=base_agent.get_co_author_relationships('Jiaxuan You',3)
