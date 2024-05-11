84492ef36ee1ea6f3a2f9ba840ab3d822f581c71
timsanders256 <73690408+timsanders256@users.noreply.github.com>
2024-05-11 22:47:08 +0800

    support write abstract method for agent (#25)
    * Implemented write abstract method for agent
    
    * fix merge error
    
    * add communicate func
    
    ---------
    
    Co-authored-by: Haofei Yu <1125027232@qq.com>

--- tests/test_agent_base.py
+++ tests/test_agent_base.py
@@ -14,6 +14,12 @@ def test_communicate():
     assert isinstance(response, str)
     assert response != ""
 
+def test_write_paper_abstract():
+    research_agent = BaseResearchAgent("Jiaxuan You")
+    abstract = research_agent.write_paper(["We can simulate the scientific research pipeline with agents."], {"2024-04":{"abstract":["Believable proxies of human behavior can empower interactive applications ranging from immersive environments to rehearsal spaces for interpersonal communication to prototyping tools. In this paper, we introduce generative agents--computational software agents that simulate believable human behavior. Generative agents wake up, cook breakfast, and head to work; artists paint, while authors write; they form opinions, notice each other, and initiate conversations; they remember and reflect on days past as they plan the next day. To enable generative agents, we describe an architecture that extends a large language model to store a complete record of the agent's experiences using natural language, synthesize those memories over time into higher-level reflections, and retrieve them dynamically to plan behavior. We instantiate generative agents to populate an interactive sandbox environment inspired by The Sims, where end users can interact with a small town of twenty five agents using natural language. In an evaluation, these generative agents produce believable individual and emergent social behaviors: for example, starting with only a single user-specified notion that one agent wants to throw a Valentine's Day party, the agents autonomously spread invitations to the party over the next two days, make new acquaintances, ask each other out on dates to the party, and coordinate to show up for the party together at the right time. We demonstrate through ablation that the components of our agent architecture--observation, planning, and reflection--each contribute critically to the believability of agent behavior. By fusing large language models with computational, interactive agents, this work introduces architectural and interaction patterns for enabling believable simulations of human behavior. "]}})
+    assert isinstance(abstract, str)
+    assert abstract != ""
+
 '''
 def test_read_paper():
     external_data = {"2021-01-01": {"abstract": ["This is a paper"]}}
