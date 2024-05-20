0ddcb39e81d4ef75618cdff701924aa932adfabe
Zhaochen Hong <zhong42@illinois.edu>
2024-05-20 03:06:26 +0000

    mock testing for idea generation

--- research_town/agents/agent_base.py
+++ research_town/agents/agent_base.py
@@ -188,7 +188,7 @@ class BaseResearchAgent(object):
             papers_embedding = get_bert_embedding(papers)
             time_chunks_embed[time] = papers_embedding
 
-        trends = summarize_research_field(
+        trends = summarize_research_field_prompting(
             profile=self.profile,
             keywords=[domain],
             dataset=dataset,
