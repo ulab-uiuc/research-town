2bf76ca6dc26c84791c8d1008c128fd4863fc3c8
Haofei Yu <1125027232@qq.com>
2024-05-22 17:36:11 -0400

    rewrite find related papers

--- tests/test_agent_base.py
+++ tests/test_agent_base.py
@@ -1,16 +1,16 @@
-from typing import Any, List
 from unittest.mock import MagicMock, patch
 
 from research_town.agents.agent_base import BaseResearchAgent
+from tests.utils import mock_papers, mock_prompting
 
 
 @patch("research_town.utils.agent_prompter.openai_prompting")
 def test_get_profile(mock_openai_prompting: MagicMock) -> None:
-    mock_response = MagicMock()
-    mock_response.return_value = [
+    mock_prompting = MagicMock()
+    mock_prompting.return_value = [
         "I am a research agent who is interested in machine learning."]
 
-    mock_openai_prompting.return_value = mock_response
+    mock_openai_prompting.return_value = mock_prompting
 
     research_agent = BaseResearchAgent("Jiaxuan You")
     profile = research_agent.profile
@@ -32,15 +32,8 @@ def test_make_review_decision(mock_openai_prompting: MagicMock) -> None:
 
 @patch("research_town.utils.agent_prompter.openai_prompting")
 def test_review_paper(mock_openai_prompting: MagicMock) -> None:
-    def mock_response(*args: Any, **kwargs: Any) -> List[str]:
-        prompt = args[1]
-        if "Please give some reviews based on the following inputs and external data." in prompt:
-            return ["This is a paper review for MambaOut."]
-        elif "Please provide a score for the following reviews." in prompt:
-            return ["2"]
-        return ["Default response"]
 
-    mock_openai_prompting.side_effect = mock_response
+    mock_openai_prompting.side_effect = mock_prompting
 
     research_agent = BaseResearchAgent("Jiaxuan You")
     score, review = research_agent.review_paper(paper={"MambaOut: Do We Really Need Mamba for Vision?": "Mamba, an architecture with RNN-like token mixer of state space model (SSM), was recently introduced to address the quadratic complexity of the attention mechanism and subsequently applied to vision tasks. Nevertheless, the performance of Mamba for vision is often underwhelming when compared with convolutional and attention-based models. In this paper, we delve into the essence of Mamba, and conceptually conclude that Mamba is ideally suited for tasks with long-sequence and autoregressive characteristics. For vision tasks, as image classification does not align with either characteristic, we hypothesize that Mamba is not necessary for this task; Detection and segmentation tasks are also not autoregressive, yet they adhere to the long-sequence characteristic, so we believe it is still worthwhile to explore Mamba's potential for these tasks. To empirically verify our hypotheses, we construct a series of models named \\emph{MambaOut} through stacking Mamba blocks while removing their core token mixer, SSM. Experimental results strongly support our hypotheses. Specifically, our MambaOut model surpasses all visual Mamba models on ImageNet image classification, indicating that Mamba is indeed unnecessary for this task. As for detection and segmentation, MambaOut cannot match the performance of state-of-the-art visual Mamba models, demonstrating the potential of Mamba for long-sequence visual tasks."})
@@ -60,22 +53,11 @@ def test_generate_idea(
     mock_get_related_papers: MagicMock,
     mock_openai_prompting: MagicMock,
 ) -> None:
-    def mock_papers(corpus: List[str], query: str, num: int) -> List[str]:
-        if "Could you summarize the keywords of high level research backgrounds and trends" in query:
-            return ["Mock abstract 1", "Mock abstract 2"]
-        return ["Default abstract"]
 
-    def mock_response(*args: Any, **kwargs: Any) -> List[str]:
-        prompt = args[1]
-        if "Please give some reviews based on the following inputs and external data." in prompt:
-            return ["This is a paper review for MambaOut."]
-        elif "Please provide a score for the following reviews." in prompt:
-            return ["2"]
-        return ["Default response"]
 
     # Configure the mocks
     mock_get_related_papers.side_effect = mock_papers
-    mock_openai_prompting.side_effect = mock_response
+    mock_openai_prompting.side_effect = mock_prompting
 
     research_agent = BaseResearchAgent("Jiaxuan You")
     ideas = research_agent.generate_idea({"2024-04": {"abstract": ["Believable proxies of human behavior can empower interactive applications ranging from immersive environments to rehearsal spaces for interpersonal communication to prototyping tools. In this paper, we introduce generative agents--computational software agents that simulate believable human behavior. Generative agents wake up, cook breakfast, and head to work; artists paint, while authors write; they form opinions, notice each other, and initiate conversations; they remember and reflect on days past as they plan the next day. To enable generative agents, we describe an architecture that extends a large language model to store a complete record of the agent's experiences using natural language, synthesize those memories over time into higher-level reflections, and retrieve them dynamically to plan behavior. We instantiate generative agents to populate an interactive sandbox environment inspired by The Sims, where end users can interact with a small town of twenty five agents using natural language. In an evaluation, these generative agents produce believable individual and emergent social behaviors: for example, starting with only a single user-specified notion that one agent wants to throw a Valentine's Day party, the agents autonomously spread invitations to the party over the next two days, make new acquaintances, ask each other out on dates to the party, and coordinate to show up for the party together at the right time. We demonstrate through ablation that the components of our agent architecture--observation, planning, and reflection--each contribute critically to the believability of agent behavior. By fusing large language models with computational, interactive agents, this work introduces architectural and interaction patterns for enabling believable simulations of human behavior. "]}}, domain="machine learning")
@@ -104,17 +86,24 @@ def test_write_paper_abstract(mock_openai_prompting: MagicMock) -> None:
     assert isinstance(abstract, str)
     assert abstract != ""
 
-
-
-
+# =========================================================
+# !IMPORTANT!
+# patch should not add path that it comes from
+# patch should add path that the function is used
+# =========================================================
 @patch("research_town.utils.agent_prompter.openai_prompting")
-def test_read_paper(mock_openai_prompting: MagicMock) -> None:
-    mock_openai_prompting.return_value = ["This is a paper"]
+@patch("research_town.utils.agent_prompter.get_related_papers")
+def test_read_paper(
+    mock_get_related_papers: MagicMock,
+    mock_openai_prompting: MagicMock,
+) -> None:
+    mock_get_related_papers.side_effect = mock_papers
+    mock_openai_prompting.side_effect = mock_prompting
 
-    external_data = {"2021-01-01": {"abstract": ["This is a paper"]}}
+    papers = {"2021-01-01": {"abstract": ["This is a paper"]}}
     domain = "machine learning"
     research_agent = BaseResearchAgent("Jiaxuan You")
-    summary = research_agent.read_paper(external_data, domain)
+    summary = research_agent.read_paper(papers, domain)
     assert isinstance(summary, str)
 
 
