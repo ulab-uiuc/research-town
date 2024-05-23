f2da29b4269bf75b0b51d408fff885d67d78f059
Zhaochen Hong <zhong42@illinois.edu>
2024-05-23 19:16:08 +0000

    support collaboration of ideas

--- research_town/envs/env_paper_submission.py
+++ research_town/envs/env_paper_submission.py
@@ -1,5 +1,7 @@
 from typing import Dict
 
+from research_town.agents.agent_base import BaseResearchAgent
+
 from .env_base import BaseMultiAgentEnv
 
 
@@ -11,12 +13,23 @@ class PaperSubmissionMultiAgentEnvironment(BaseMultiAgentEnv):
 
     def step(self) -> None:
         external_data = self.kb.get_data(10, "machine learning")
+        agent_names_to_objs = {self.agents[iter_agent].name: self.agents[iter_agent] for iter_agent in self.agents} # map from real names to agent objects
         abstracts = {}
         for agent_name, agent in self.agents.items():
             trend = agent.read_paper(papers=external_data, domain="machine learning")
             trends = [trend]
-            agent.find_collaborators(self.task)
+            collaborators = agent.find_collaborators(self.task)
+            collaborator_agents = []
+            for researcher_name in collaborators:
+                if researcher_name not in agent_names_to_objs:
+                    new_agent_obj = BaseResearchAgent(researcher_name)
+                    collaborator_agents.append(new_agent_obj)
+                    agent_names_to_objs[researcher_name] = new_agent_obj
+                else:
+                    collaborator_agents.append(agent_names_to_objs[researcher_name])
             ideas = agent.generate_idea(trends=trends, domain="machine learning")
+            for collaborator_agent in collaborator_agents:
+                ideas.extend(collaborator_agent.generate_idea(trends=trends, domain="machine learning"))
             abstract = agent.write_paper(ideas, external_data)
             abstracts[agent_name] = abstract
         self.kb.update_kb(abstracts)
