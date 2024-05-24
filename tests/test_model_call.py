6973fe4457a4d7cafb6c286baf6177c25fb1c898
Jinwei <769845882@qq.com>
2024-05-24 15:51:06 +0800

    rename test_model_call

--- tests/test_model_call.py
+++ tests/test_model_call.py
@@ -1,7 +1,7 @@
 from research_town.utils.agent_prompter import model_prompting
 
 
-def test_openai_call() -> None:
+def test_model_call() -> None:
     prompt = "Here is a high-level summarized trend of a research field Machine Learning. "
     response = model_prompting("mistralai/Mixtral-8x7B-Instruct-v0.1", prompt)
     assert response is not None
