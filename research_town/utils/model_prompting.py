f36d3e4befa58bea8af3cc1a6a2266fff7ce0215
Jinwei <55192557+Monstertail@users.noreply.github.com>
2024-05-26 07:39:37 +0800

    Reorg/seperate model prompting api (#95)
    * Seperate model_prompting and support more params
    
    * fix mypt error of litellm
    
    * Fix issues identified by pre-commit hooks
    
    * set default return_num to 1

---
+++ research_town/utils/model_prompting.py
@@ -0,0 +1,32 @@
+from typing import List, Optional
+
+import litellm
+
+from .decorator import exponential_backoff
+
+
+@exponential_backoff(retries=5, base_wait_time=1)
+def model_prompting(
+    llm_model: str,
+    prompt: str,
+    return_num: Optional[int] = 1,
+    max_token_num: Optional[int] = 512,
+    temperature: Optional[float] = None,
+    top_p: Optional[float] = None,
+    stream: Optional[bool] = None,
+) -> List[str]:
+    """
+    Select model via router in LiteLLM.
+    """
+    completion = litellm.completion(
+    model=llm_model,
+    messages=[{"role": "user", "content": prompt}],
+    max_tokens=max_token_num,
+    n=return_num, # for some models, 'n'(The number of chat completion choices ) is not supported.
+    top_p=top_p,
+    temperature=temperature,
+    stream=stream,
+)
+    content = completion.choices[0].message.content
+    content_l = [content]
+    return content_l
