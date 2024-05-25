c23637a1787272524dc7c93436b695f9f66c8da6
Haofei Yu <1125027232@qq.com>
2024-05-25 18:25:23 -0400

    support llm name in research_agent (#94)
    * support llm name
    
    * fix pre-commti
    
    * fix mypy
    
    * fix mypy
    
    * fix mypy
    
    * change name

--- research_town/dbs/env_db.py
+++ research_town/dbs/env_db.py
@@ -73,7 +73,7 @@ class EnvLogDB:
 class AgentPaperReviewLog(BaseModel):
     pk: str = Field(default_factory=lambda: str(uuid.uuid4()))
     timestep: int = Field(default=0)
-    paper_pk: str 
+    paper_pk: str
     agent_pk: str
     review_score: Optional[int] = Field(default=0)
     review_content: Optional[str] = Field(default=None)
@@ -102,4 +102,3 @@ class AgentAgentDiscussionLog(BaseModel):
     agent_from_pk: str
     agent_to_pk: str
     message: Optional[str] = Field(default=None)
-
