b581e6969c57c5fd36ee767ba4c6034a747251fb
Jinwei <769845882@qq.com>
2024-05-26 22:23:53 +0800

    define evalouput

--- research_town/evals/eval_general_quality.py
+++ research_town/evals/eval_general_quality.py
@@ -1,7 +1,8 @@
 
 from typing import Optional, Tuple, Any
 from ..utils.eval_prompter import GeneralQuality_idea_EvalPrompting
-import eval_output
+from eval_output import parser_GeneralQuality_idea, EvalOutput_GeneralQuality
+
 class PromptBasedGeneralQualityEval:
     def __init__(self, model_name: str, progress_dic: dict, *args: Any, **kwargs: Any )-> None:
         self.model_name = model_name
@@ -13,17 +14,17 @@ class PromptBasedGeneralQualityEval:
             if key not in progress_dic:
                 raise ValueError(f"Missing required key in progress dic: {key}")
         self.progress2eval = {key: progress_dic[key] for key in required_keys}
-        self.eval_res = None # to store the result of evaluation
+        self.eval_res = EvalOutput_GeneralQuality() # to store the result of evaluation
 
     
     def eval_idea(self)-> None:
         # generate the prompt template and prompting (prompter in eval_prompter.py)
         model_output = GeneralQuality_idea_EvalPrompting(ideas=self.progress2eval['idea'],model_name=self.model_name)
-        # parse the prompting output(parser in eval_out.py)
-
+        # parse the prompting output(parser in eval_out.py). Extract a score and text in List[Tuple(float, str)].
+        parsed_idea_eval = parser_GeneralQuality_idea(idea_output=model_output)
         # store it to self.eval_res
-
-        pass
+        self.eval_res.idea = parsed_idea_eval
+        
 
     def eval_paper(self)-> None:
         pass
--- research_town/evals/eval_output.py
+++ research_town/evals/eval_output.py
@@ -1,5 +1,15 @@
 from pydantic import BaseModel, Field, validator
 
+from typing import Optional, Tuple, List
 # define output format and output parser
 
 # Output format. Refer to: https://github.com/sotopia-lab/sotopia/blob/2227503f5091961041ddb1da5b7c7836febfa650/sotopia/envs/evaluators.py#L20 .
+
+class EvalOutput_GeneralQuality:
+    def __init__(self) -> None:
+        self.idea = None
+        self.paper = None
+        self.reviw = None
+        self.disc = None
+def parser_GeneralQuality_idea(idea_output:List[str])->List[Tuple(float,str)]:
+    pass
