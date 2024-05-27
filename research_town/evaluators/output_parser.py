a257f9619ad26fe089e51a5408b0bc5c90b3c9ba
Haofei Yu <1125027232@qq.com>
2024-05-27 17:07:20 -0400

    reorg parser

--- research_town/evaluators/output_parser.py
+++ research_town/evaluators/output_parser.py
@@ -1,40 +1,25 @@
 import re
 from typing import List
+from pydantic import BaseModel, Field
 
-# define output format and output parser
 
-# Output format. Refer to: https://github.com/sotopia-lab/sotopia/blob/2227503f5091961041ddb1da5b7c7836febfa650/sotopia/envs/evaluators.py#L20 .
+class IdeaEvalOutput(BaseModel):
+    overall_score: int = Field(default=-1)
+
+class PaperEvalOutput(BaseModel):
+    overall_score: int = Field(default=-1)
 
 class EvalOutputParser(object):
-    def __init__(self) -> None:
-        self.idea_score: List[int] = []
-        self.paper_score: List[int] = []
-        self.reviw_score: List[int] = []
+    def parse_idea_score(self, raw_output:str) -> IdeaEvalOutput:
+        match = re.search(r"Overall\s*Score\s*\W*(\d+)\W*", raw_output, re.IGNORECASE)
+        if match:
+            return PaperEvalOutput(overall_score=int(match.group(1)))
+        else:
+            return PaperEvalOutput()
 
-    def parse_idea_score(self,idea_output:List[str])->List[int]:
-        # idea_output format: a list of string like "Overall Score=89. Dimension Scores=[8,9,9,9,9,9,9,9,9,9]"
-        default_score = int(-1)
-        overall_scores = []
-        for output in idea_output:
-            match = re.search(r"Overall\s*Score\s*\W*(\d+)\W*", output, re.IGNORECASE)
-            if match:
-                score = int(match.group(1))
-                overall_scores.append(score)
-            else:
-                overall_scores.append(default_score)
-        self.idea_score = overall_scores
-        return self.idea_score
-
-    def parse_paper_score(self,paper_output:List[str])->List[int]:
-        # paper_output format: a list of string like "Overall Score=89. Dimension Scores=[8,9,9,9,9,9,9,9,9,9]"
-        default_score = int(-1)
-        overall_scores = []
-        for output in paper_output:
-            match = re.search(r"Overall\s*Score\s*\W*(\d+)\W*", output, re.IGNORECASE)
-            if match:
-                score = int(match.group(1))
-                overall_scores.append(score)
-            else:
-                overall_scores.append(default_score)
-        self.paper_score = overall_scores
-        return self.paper_score
+    def parse_paper_score(self, raw_output:str) -> PaperEvalOutput:
+        match = re.search(r"Overall\s*Score\s*\W*(\d+)\W*", raw_output, re.IGNORECASE)
+        if match:
+            return PaperEvalOutput(overall_score=int(match.group(1)))
+        else:
+            return PaperEvalOutput()
--- research_town/evaluators/quality_evaluator.py
+++ research_town/evaluators/quality_evaluator.py
@@ -1,5 +1,5 @@
 
-from typing import Any, Dict, List, Tuple, Union
+from typing import Any, Dict, List, Optional
 
 from ..utils.eval_prompter import (
     idea_quality_eval_prompting,
@@ -11,45 +11,40 @@ from .output_parser import EvalOutputParser
 class QualityEvaluator(object):
     def __init__(self,
         model_name: str,
-        progress_dict: Dict[str, Union[Dict[str, str], Dict[str, Tuple[str, str]]]],
+        parser: Optional[EvalOutputParser] = EvalOutputParser(),
         *args: Any,
         **kwargs: Any
     )-> None:
         self.model_name = model_name
-        self.prompt = ""
-        # progress with info in a dict to evaluate
-        # keys: 1) idea; 2) paper; 3) review; 4) discussion.
-        required_keys = ['idea', 'paper', 'review', 'discussion','trend']
-        for key in required_keys:
-            if key not in progress_dict:
-                raise ValueError(f"Missing required key in progress dic: {key}")
-        self.progress2eval = {key: progress_dict[key] for key in required_keys}
-        self.parser = EvalOutputParser() # to store the result of evaluation
+        self.parser = parser
 
-
-    def eval_idea(self)-> List[int]:
-        # generate the prompt template and prompting (prompter in eval_prompter.py)
-        model_output = idea_quality_eval_prompting(
-            ideas=self.progress2eval['idea'],
-            trends=self.progress2eval['trend'],
+    def eval_idea(
+        self, 
+        idea: Dict[str], 
+        trend: Dict[str], 
+        *args, 
+        **kwargs
+    )-> List[int]:
+        raw_output = idea_quality_eval_prompting(
+            ideas=idea,
+            trends=trend,
             model_name=self.model_name
         )
-        # parse the prompting output(parser in eval_out.py). Extract a score in List[int]. Tuple(overall score, soundness, insightful, novelty, practial..)
-        parsed_idea_eval = self.parser.parse_idea_score(
-            idea_output=model_output
-        )
-        return parsed_idea_eval
+        parsed_output = self.parser.parse_idea_score(raw_output)
+        return parsed_output
 
-    def eval_paper(self)-> List[int]:
-        model_output = paper_quality_eval_prompting(
-            ideas=self.progress2eval['idea'],
-            papers=self.progress2eval['paper'],
+    def eval_paper(
+        self, 
+        idea: Dict[str], 
+        paper: Dict[str, str]
+    )-> List[int]:
+        raw_output = paper_quality_eval_prompting(
+            ideas=idea,
+            papers=paper,
             model_name=self.model_name
         )
-        # parse the prompting output(parser in eval_out.py). Extract a score in List[int]. Tuple(overall score, soundness, insightful, novelty, practial..)
-        parsed_paper_eval = self.parser.parse_paper_score(paper_output=model_output)
-        return parsed_paper_eval
+        parsed_output = self.parser.parse_paper_score(raw_output)
+        return parsed_output
 
     def eval_review(self)-> None:
-        # component/stage evaluation: compare with open review by ranking consistency
         pass
