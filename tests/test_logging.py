302cae4cdba0f5cd6dd291fea877d88ca2ae5a93
Zhaochen Hong <zhong42@illinois.edu>
2024-05-29 06:03:02 +0000

    rename logger.py

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,4 +1,6 @@
-from research_town.utils.logging_callback import logging_callback
+from research_town.agents.agent_base import BaseResearchAgent
+from research_town.utils.logging import logging_callback
+from tests.constants import agent_profile_A
 
 
 def test_logging_callback() -> None:
