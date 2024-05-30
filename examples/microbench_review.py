c9970cc688d14d30e9c89fab9c3d3cb6c4521901
Jinwei <769845882@qq.com>
2024-05-30 22:08:52 +0800

    init microbench_review

---
+++ examples/microbench_review.py
@@ -0,0 +1,17 @@
+
+
+
+def main() -> None:
+    # collect papers from openreview
+
+    # generate envs of agents with reviewers
+
+    # review papers
+
+    # get ranking consistency
+    
+    pass
+
+
+if __name__ == '__main__':
+    main()
