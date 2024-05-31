0d3360f3c6aa85e841b14158f7f5c20a22ecba8b
Haofei Yu <1125027232@qq.com>
2024-05-30 21:53:28 -0400

    fix

--- examples/microbench_review.py
+++ examples/microbench_review.py
@@ -1,17 +1,15 @@
 
-import argparse
-import json
-import os
-import uuid
-from typing import Dict, List, Optional
-
-from pydantic import BaseModel, Field
-from tqdm import tqdm
-
-from research_town.agents.agent_base import BaseResearchAgent
 from research_town.dbs.agent_db import AgentProfile, AgentProfileDB
-from research_town.dbs.env_db import AgentPaperReviewLog
 from research_town.dbs.paper_db import PaperProfile, PaperProfileDB
+from research_town.dbs.env_db import AgentPaperReviewLog
+from research_town.agents.agent_base import BaseResearchAgent
+from typing import List, Optional, Dict
+from pydantic import BaseModel, Field
+import os
+import json
+import argparse
+from tqdm import tqdm
+import uuid
 
 
 class RealPaperWithReview (BaseModel): # paper review from real reviewers
@@ -20,7 +18,6 @@ class RealPaperWithReview (BaseModel): # paper review from real reviewers
     abstract: Optional[str] = Field(default=None)
     authors: Optional[List[str]] = Field(default=[])
     keywords: Optional[List[str]] = Field(default=None)
-<<<<<<< HEAD
 
     real_avg_scores: float = Field(default=-1)
     real_all_scores: List[int] = Field(default=[])
@@ -37,50 +34,9 @@ class RealPaperWithReview (BaseModel): # paper review from real reviewers
 
 class RealPaperWithReviewDB(object):
     def __init__(self) -> None:
-||||||| 8ab953c
-    # real reviews
-    real_avg_scores: Optional[float] = Field(default=None)
-    real_all_scores: Optional[List[int]] = Field(default=[])
-    real_contents: Optional[List[str]] = Field(default=[])
-    real_rank: Optional[int] = Field(default=0)
-    real_decision: Optional[str] = Field(default=None)
-    # simulated reviews
-    sim_avg_scores: Optional[float] = Field(default=None) # from the simulation
-    sim_all_scores: Optional[List[int]] = Field(default=[])
-    sim_contents: Optional[List[str]] = Field(default=[])
-    sim_rank: Optional[int] = Field(default=0)
-    sim_decision: Optional[str] = Field(default=None)
-
-class RealPaperWithReviewDB:
-    def __init__(self):
-=======
-    # real reviews
-    real_avg_scores: Optional[float] = Field(default=None)
-    real_all_scores: Optional[List[int]] = Field(default=[])
-    real_contents: Optional[List[str]] = Field(default=[])
-    real_rank: Optional[int] = Field(default=0)
-    real_decision: Optional[str] = Field(default=None)
-    # simulated reviews
-    sim_avg_scores: Optional[float] = Field(default=None) # from the simulation
-    sim_all_scores: Optional[List[int]] = Field(default=[])
-    sim_contents: Optional[List[str]] = Field(default=[])
-    sim_rank: Optional[int] = Field(default=0)
-    sim_decision: Optional[str] = Field(default=None)
-
-class RealPaperWithReviewDB:
-    def __init__(self)-> None:
->>>>>>> eb5f7ba5d67cb085a4684c1dff80dce5fe9d5e5c
         self.data: Dict[str, RealPaperWithReview] = {}
         self.rank_consistency:float = 0.0
-<<<<<<< HEAD
 
-||||||| 8ab953c
-
-=======
-        self.sim_ranks: List[int] = []
-        self.real_ranks: List[int] = []
-
->>>>>>> eb5f7ba5d67cb085a4684c1dff80dce5fe9d5e5c
     def add(self, real_paper: RealPaperWithReview) -> None:
         self.data[real_paper.title] = real_paper
 
@@ -91,15 +47,13 @@ class RealPaperWithReviewDB:
                 title: RealPaperWithReview(**details)
                 for title, details in raw_data.items()
             }
-
+    
     def save_to_file(self, file_name: str) -> None:
         combined_data = {
             "rank_consistency": self.rank_consistency,
-            'sim_ranks': self.sim_ranks,
-            'real_ranks': self.real_ranks,
             "papers": {title: real_paper.dict() for title, real_paper in self.data.items()}
         }
-
+        
         with open(file_name, 'w') as f:
             json.dump(combined_data, f, indent=2)
 
@@ -125,7 +79,7 @@ class RealPaperWithReviewDB:
                         real_paper.sim_all_scores.append(agent_review.review_score)
                     if agent_review.review_content is not None:
                         real_paper.sim_contents.append(agent_review.review_content)
-
+        
         for real_paper in self.data.values():
             assert  len(real_paper.real_all_scores) > 0, f"no real review score for paper {real_paper.paper_pk} with title of {real_paper.title}"
             real_paper.real_avg_scores = sum(real_paper.real_all_scores) / len(real_paper.real_all_scores)
@@ -140,20 +94,18 @@ class RealPaperWithReviewDB:
         real_papers.sort(key=lambda x: x.sim_avg_scores, reverse=True)
         for i, real_paper in enumerate(real_papers):
             real_paper.sim_rank = i + 1
-
+        
     def calculate_rank_consistency(self) -> float:
         rank_consistency_float = 0.
         for real_paper in self.data.values():
             rank_consistency_float += abs(real_paper.real_rank - real_paper.sim_rank)
-            self.sim_ranks.append(real_paper.sim_rank)
-            self.real_ranks.append(real_paper.real_rank)
         rank_consistency_float = rank_consistency_float / len(self.data)
 
         self.rank_consistency = rank_consistency_float
         return rank_consistency_float
 
 
-def main(data_path: str, domain:str, model_name:str, review_agent_num: int) -> None:
+def main(data_path: str, domain:str) -> None:
     print(f"Data path is: {data_path}")
     print(f"Domain is: {domain}")
     # collect papers from openreview
@@ -170,9 +122,8 @@ def main(data_path: str, domain:str, model_name:str, review_agent_num: int) -> N
     # (jinwei) Hardcode-- select top 3 reviewers in the agent_db to agent_profiles
     agent_profiles: List[AgentProfile] = []
     all_reviewers = list(agent_db.data.values()) # Convert dict_values to a list
-
-    reviewer_num = review_agent_num  if review_agent_num<=len(all_reviewers) else len(all_reviewers)
-    for i in range(reviewer_num):
+    
+    for i in range(3):
         agent_profiles.append(all_reviewers[i])
     # create agents
     agents: List[BaseResearchAgent] = []
@@ -180,10 +131,10 @@ def main(data_path: str, domain:str, model_name:str, review_agent_num: int) -> N
             agents.append(
                 BaseResearchAgent(
                     agent_profile=agent_profile,
-                    model_name=model_name,
+                    model_name='together_ai/mistralai/Mixtral-8x7B-Instruct-v0.1',
                 )
             )
-
+    
     # review papers
     # 3. how to get the scores of papers? Store to review log lists.
     reviews: List[AgentPaperReviewLog] = []
@@ -192,57 +143,40 @@ def main(data_path: str, domain:str, model_name:str, review_agent_num: int) -> N
         # Inner loop (papers) with tqdm
         for paper in tqdm(Papers2eval, desc="Paper Review Progress"):
             reviews.append(agent.write_paper_review(paper=paper))
-
+    
     # get ranking consistency
     real_paper_db.map_agent_reviews_to_real_paper(reviews)
     rank_consistency = real_paper_db.calculate_rank_consistency()
     # print rank consistency
     print(f"rank_consistency = {rank_consistency}\n")
-    print(f"real_ranks = {real_paper_db.real_ranks}\n")
-    print(f"sim_ranks = {real_paper_db.sim_ranks}\n")
     # save the RealPaperWithReviewDB
     # Construct the output file path
     output_file = os.path.join(data_path, f"output_microbench_review_{domain}.json")
     real_paper_db.save_to_file(output_file)
-
+    
 
 
 if __name__ == '__main__':
     # Get the directory of the current script
     current_dir = os.path.dirname(os.path.abspath(__file__))
-
+    
     # Construct the path to data/microbench
     default_data_path = os.path.join(current_dir, '..', 'data', 'microbench')
 
     parser = argparse.ArgumentParser(description="Process folder path of microbench.")
     parser.add_argument(
-        "--data_path",
-        type=str,
-        default=default_data_path,
+        "--data_path", 
+        type=str, 
+        default=default_data_path, 
         help="Path to the data directory for microbenchmark."
     )
     # Add argument for domain
     parser.add_argument(
-        "--domain",
-        type=str,
-        default="machine_learning_system",
+        "--domain", 
+        type=str, 
+        default="machine_learning_system", 
         help="Domain of papers to be reviewed."
     )
-
-    # Add argument for models
-    parser.add_argument(
-        "--model_name",
-        type=str,
-        default='together_ai/mistralai/Mixtral-8x7B-Instruct-v0.1',
-        help="Models for reviewers."
-    )
-
-    # Add argument for review_agent_num
-    parser.add_argument(
-        "--review_agent_num",
-        type=int,
-        default=3,
-        help="Number of total reviewers."
-    )
+    
     args = parser.parse_args()
-    main(args.data_path, args.domain, args.model_name, args.review_agent_num)
+    main(args.data_path, args.domain)
