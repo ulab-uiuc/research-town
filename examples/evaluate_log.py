b719efde3bafffe7a6b0351e26fdc8bbd5f51dd1
chengzr01 <ziruicheng01@gmail.com>
2024-05-31 10:11:45 +0800

    add sync evaluation

--- examples/evaluate_log.py
+++ examples/evaluate_log.py
@@ -1,3 +1,12 @@
+from research_town.dbs import (
+    ResearchIdea,  # Idea
+    ResearchInsight,
+    ResearchPaperSubmission,  # Paper
+    AgentPaperReviewLog,  # Review
+    AgentPaperMetaReviewLog,
+    AgentPaperRebuttalLog,
+)
+
 from research_town.evaluators import (
     IdeaQualityEvaluator,
     PaperQualityEvaluator,
@@ -9,6 +18,9 @@ from research_town.evaluators import (
 
 
 def run_sync_evaluation():
+    idea_quality_evaluator = IdeaQualityEvaluator(model_name="gpt-4")
+    paper_quality_evaluator = PaperQualityEvaluator(model_name="gpt-4")
+    review_quality_evaluator = ReviewQualityEvaluator(model_name="gpt-4")
     pass
 
 
