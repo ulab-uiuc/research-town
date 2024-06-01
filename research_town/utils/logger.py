9919d9cd849c1814281fe80bf9c902800d9c26d2
Haofei Yu <1125027232@qq.com>
2024-06-01 12:17:11 -0400

    modify logger

--- research_town/utils/logger.py
+++ research_town/utils/logger.py
@@ -1,15 +1,28 @@
 import logging
+import colorlog
 
 from beartype.typing import Any, Callable, Dict, List, Union
 
 app_logger = logging.getLogger('research_town')
 app_logger.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
-console_formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+console_formatter = colorlog.ColoredFormatter(
+    "%(log_color)s%(levelname)s:%(name)s:%(message)s",
+    datefmt=None,
+    reset=True,
+    log_colors={
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    },
+    secondary_log_colors={},
+    style='%'
 )
 console_handler.setFormatter(console_formatter)
 app_logger.addHandler(console_handler)
+
 LogType = Union[List[Dict[str, str]], None]
 
 
