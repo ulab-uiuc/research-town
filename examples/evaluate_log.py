7afea7ad3c0742ca07607a774e214d3357b5241a
chengzr01 <ziruicheng01@gmail.com>
2024-06-01 23:15:09 +0800

    update evaluate log

--- examples/evaluate_log.py
+++ examples/evaluate_log.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 from research_town.dbs import (
     ResearchIdea,  # Idea
     ResearchInsight,
@@ -17,7 +19,7 @@ from research_town.evaluators import (
 )
 
 
-def run_sync_evaluation():
+def run_sync_evaluation(insight_list: List[ResearchInsight], idea_list: List[ResearchIdea],  paper_list: List[ResearchPaperSubmission], review_list: List[AgentPaperReviewLog]):
     idea_quality_evaluator = IdeaQualityEvaluator(model_name="gpt-4")
     paper_quality_evaluator = PaperQualityEvaluator(model_name="gpt-4")
     review_quality_evaluator = ReviewQualityEvaluator(model_name="gpt-4")
