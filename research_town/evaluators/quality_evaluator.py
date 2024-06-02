b14231cbd2923a2bf7e0822133ad6d41fd3b075e
Jinwei <769845882@qq.com>
2024-06-02 09:01:39 +0800

    adds more interfaces for evaluator  prompts

--- research_town/evaluators/quality_evaluator.py
+++ research_town/evaluators/quality_evaluator.py
@@ -82,7 +82,7 @@ class PaperQualityEvaluator(object):
         **kwargs: Any,
     ) -> PaperEvalOutput:
         raw_output = paper_quality_eval_prompting(
-            idea=kwargs['idea'], paper=kwargs['paper'], model_name=self.model_name
+            idea=kwargs['idea'], paper=kwargs['paper'], model_name=self.model_name,trend= kwargs['trend'] if 'trend' in kwargs else None
         )
         self.parsed_output = self.parse(raw_output)
 
@@ -139,9 +139,11 @@ class ReviewQualityEvaluator(object):
             idea=kwargs['idea'],  # idea: str,
             trend=kwargs['trend'],  # trend: str,
             paper=kwargs['paper'],  # paper: Dict[str,str],
-            review=kwargs['review'],  # review: Dict[str,str],
+            review=kwargs['review'],  # review: List[str],
             decision=kwargs['decision'],  # decision: str,
             model_name=self.model_name,
+            rebuttal=kwargs['rebuttal'] if 'rebuttal' in kwargs else None, # rebuttal: str,
+            meta_review=kwargs['meta_review'] if 'meta_review' in kwargs else None, # meta_review: str,
         )
         self.parsed_output = self.parse(raw_output)
         # Store the input kwargs in parsed_output
