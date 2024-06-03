0bd0ca80057db2edac600340a4e832283d313808
chengzr01 <ziruicheng01@gmail.com>
2024-06-03 19:16:43 +0800

    add more domain data

--- scripts/run.py
+++ scripts/run.py
@@ -5,14 +5,17 @@ from research_town.utils.agent_collector import fetch_author_info
 
 agent_db = AgentProfileDB()
 
-domain = 'computer vision'
+domain = 'federated learning'
+domain_path = domain.replace(' ', '_')
+print(domain_path)
+
 initial_list = [
-    'Kaiming He',
-    'Ross Girshick',
-    'Li Fei-Fei',
-    'Jia Deng',
-    'Ali Farhadi',
-    'Richard Szeliski',
+    'Daniel Ramage',
+    'H. Brendan McMahan',
+    'Song Guo',
+    'Yiran Chen',
+    'Baochun Li',
+    'Nicholas Lane',
 ]
 
 final_list = []
@@ -26,11 +29,10 @@ final_list.extend(initial_list)
 final_list = list(set(final_list))
 
 agent_db.fetch_and_add_agents(final_list)
+
 agent_db.save_to_file('data/agent_data/' + domain + '.json')
 
 paper_db = PaperProfileDB()
-
-domain = 'computer vision'
 
 paper_db.fetch_and_add_papers(10, domain)
 
