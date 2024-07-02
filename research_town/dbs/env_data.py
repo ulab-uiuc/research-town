9ae1d1e29dcd0c00360acd99c9bbfe257876fc7b
lwaekfjlk <1125027232@qq.com>
2024-07-02 15:33:11 -0400

    add more classes

--- research_town/dbs/env_data.py
+++ research_town/dbs/env_data.py
@@ -1,7 +1,49 @@
 import uuid
-from typing import Optional
+from typing import List, Optional
 
 from pydantic import BaseModel, Field
+
+
+class AgentPaperLiteratureReviewLog(BaseModel):
+    pk: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    timestep: int = Field(default=0)
+    paper_pk: str
+    agent_pk: str
+    insight_pks: Optional[List[str]] = Field(default=[])
+    other_agent_pks: Optional[List[str]] = Field(default=[])
+
+
+class AgentIdeaBrainstormingLog(BaseModel):
+    pk: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    timestep: int = Field(default=0)
+    idea_pk: str
+    agent_pk: str
+    other_agent_pks: Optional[List[str]] = Field(default=[])
+
+
+class AgentAgentCollaborationFindingLog(BaseModel):
+    pk: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    timestep: int = Field(default=0)
+    agent_pk: str
+    other_agent_pks: Optional[List[str]] = Field(default=[])
+
+
+class AgentAgentIdeaDiscussionLog(BaseModel):
+    pk: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    timestep: int = Field(default=0)
+    agent_from_pk: str
+    agent_from_name: str
+    agent_to_pk: str
+    agent_to_name: str
+    message: Optional[str] = Field(default=None)
+
+
+class AgentPaperSubmissionLog(BaseModel):
+    pk: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    timestep: int = Field(default=0)
+    paper_pk: str
+    agent_pk: str
+    other_agent_pks: Optional[List[str]] = Field(default=[])
 
 
 class AgentPaperReviewLog(BaseModel):
@@ -9,6 +51,7 @@ class AgentPaperReviewLog(BaseModel):
     timestep: int = Field(default=0)
     paper_pk: str
     agent_pk: str
+    other_agent_pks: Optional[List[str]] = Field(default=[])
     review_score: Optional[int] = Field(default=0)
     review_content: Optional[str] = Field(default=None)
 
@@ -18,6 +61,7 @@ class AgentPaperRebuttalLog(BaseModel):
     timestep: int = Field(default=0)
     paper_pk: str
     agent_pk: str
+    other_agent_pks: Optional[List[str]] = Field(default=[])
     rebuttal_content: Optional[str] = Field(default=None)
 
 
@@ -26,15 +70,6 @@ class AgentPaperMetaReviewLog(BaseModel):
     timestep: int = Field(default=0)
     paper_pk: str
     agent_pk: str
+    other_agent_pks: Optional[List[str]] = Field(default=[])
     decision: Optional[bool] = Field(default=False)
     meta_review: Optional[str] = Field(default=None)
-
-
-class AgentAgentDiscussionLog(BaseModel):
-    pk: str = Field(default_factory=lambda: str(uuid.uuid4()))
-    timestep: int = Field(default=0)
-    agent_from_pk: str
-    agent_from_name: str
-    agent_to_pk: str
-    agent_to_name: str
-    message: Optional[str] = Field(default=None)
