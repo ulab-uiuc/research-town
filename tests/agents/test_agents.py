a5f16008be213f56a2c126b7b38446851d20b0bc
Haofei Yu <1125027232@qq.com>
2024-07-07 23:24:37 -0400

    delete a useless line

--- tests/agents/test_agents.py
+++ tests/agents/test_agents.py
@@ -22,8 +22,6 @@ from tests.constants.db_constants import (
 )
 from tests.mocks.mocking_func import mock_papers, mock_prompting
 
-# ==============================================================
-
 
 @patch('research_town.utils.agent_prompter.model_prompting')
 @patch('research_town.utils.agent_prompter.get_related_papers')
