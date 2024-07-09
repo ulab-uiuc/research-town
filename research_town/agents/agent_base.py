b7a4b02296cd1d2d6eaf4263bbb7976aab0ede40
ft2023 <129666640+ft2023@users.noreply.github.com>
2024-07-09 13:43:53 +0800

    Add files via upload

--- research_town/agents/agent_base.py
+++ research_town/agents/agent_base.py
@@ -1,102 +1,88 @@
+from datetime import datetime
+
 from beartype import beartype
-from beartype.typing import Dict, List, Literal, Optional
+from beartype.typing import Any, Dict, List, Tuple
 
 from ..configs import Config
 from ..dbs import (
+    AgentAgentDiscussionLog,
+    AgentPaperMetaReviewLog,
+    AgentPaperRebuttalLog,
+    AgentPaperReviewLog,
     AgentProfile,
     PaperProfile,
     ResearchIdea,
     ResearchInsight,
-    ResearchMetaReviewForPaperSubmission,
     ResearchPaperSubmission,
-    ResearchRebuttalForPaperSubmission,
-    ResearchReviewForPaperSubmission,
 )
 from ..utils.agent_prompter import (
-    brainstorm_idea_prompting,
-    discuss_idea_prompting,
-    review_literature_prompting,
+    discuss_prompting,
+    read_paper_prompting,
+    review_paper_prompting,
+    review_score_prompting,
+    summarize_ideas_prompting,
+    think_idea_prompting,
     write_meta_review_prompting,
     write_paper_prompting,
     write_rebuttal_prompting,
-    write_review_prompting,
-)
-from ..utils.agent_role_verifier import (
-    chair_required,
-    proj_leader_required,
-    proj_participant_required,
-    reviewer_required,
 )
 from ..utils.serializer import Serializer
 
-Role = Literal['reviewer', 'proj_leader', 'proj_participant', 'chair']
-
 
 class BaseResearchAgent(object):
-    def __init__(
-        self,
-        agent_profile: AgentProfile,
-        model_name: str,
-        agent_role: Optional[Role] = None,
-    ) -> None:
+    def __init__(self, agent_profile: AgentProfile, model_name: str) -> None:
         self.profile: AgentProfile = agent_profile
         self.memory: Dict[str, str] = {}
-        self.role: Role | None = agent_role
         self.model_name: str = model_name
         self.serializer = Serializer()
 
-    @beartype
-    def assign_role(self, role: Role) -> None:
-        self.role = role
 
     @beartype
-    @proj_participant_required
-    def review_literature(
+    def literature_review(
         self, papers: List[PaperProfile], domains: List[str], config: Config
-    ) -> List[ResearchInsight]:
+    ) -> Tuple[List[ResearchInsight], List[int]]:
         serialized_papers = self.serializer.serialize(papers)
         serialized_profile = self.serializer.serialize(self.profile)
-        insight_contents = review_literature_prompting(
+        insight_contents,related_papers_idx = read_paper_prompting(
             profile=serialized_profile,
             papers=serialized_papers,
             domains=domains,
             model_name=self.model_name,
-            prompt_template_query_paper=config.prompt_template.query_paper,
-            prompt_template_review_literature=config.prompt_template.review_literature,
+            prompt_template_query=config.prompt_template.query_paper,
+            prompt_template_read=config.prompt_template.read_paper,
         )
         insights: List[ResearchInsight] = []
         for content in insight_contents:
             insights.append(ResearchInsight(content=content))
-        return insights
+        return insights,related_papers_idx
 
     @beartype
-    @proj_participant_required
-    def brainstorm_idea(
+    def idea_brainstorming(
         self, insights: List[ResearchInsight], config: Config
     ) -> ResearchIdea:
         serialized_insights = self.serializer.serialize(insights)
-        idea_content = brainstorm_idea_prompting(
+        idea_content = think_idea_prompting(
             insights=serialized_insights,
             model_name=self.model_name,
-            prompt_template=config.prompt_template.brainstorm_idea,
+            prompt_template=config.prompt_template.think_idea,
         )[0]
         return ResearchIdea(content=idea_content)
 
     @beartype
-    @proj_participant_required
-    def discuss_idea(self, ideas: List[ResearchIdea], config: Config) -> ResearchIdea:
+    def idea_discussion(
+        self, ideas: List[ResearchIdea], config: Config
+    ) -> ResearchIdea:
         serialized_ideas = self.serializer.serialize(ideas)
-        idea_summarized = discuss_idea_prompting(
+        idea_summarized = summarize_ideas_prompting(
             ideas=serialized_ideas,
             model_name=self.model_name,
-            prompt_template=config.prompt_template.discuss_idea,
+            prompt_template=config.prompt_template.summarize_ideas,
         )[0]
         return ResearchIdea(content=idea_summarized)
 
     @beartype
-    @proj_leader_required
-    def write_paper(
-        self, idea: ResearchIdea, papers: List[PaperProfile], config: Config
+    def paper_abstract_writing(
+        self, idea:  List[ResearchIdea], papers: List[PaperProfile], config: Config
     ) -> ResearchPaperSubmission:
         serialized_idea = self.serializer.serialize(idea)
         serialized_papers = self.serializer.serialize(papers)
@@ -109,72 +95,60 @@ class BaseResearchAgent(object):
         return ResearchPaperSubmission(abstract=paper_abstract)
 
     @beartype
-    @reviewer_required
-    def write_review(
-        self, paper: ResearchPaperSubmission, config: Config
-    ) -> ResearchReviewForPaperSubmission:
+    def write_paper_review(
+        self, profile:AgentProfile,paper: ResearchPaperSubmission, config: Config
+    ) -> AgentPaperReviewLog:
         serialized_paper = self.serializer.serialize(paper)
-
-        summary, strength, weakness, score = write_review_prompting(
+        serialized_profile=self.serializer.serialize(profile)
+        paper_review = review_paper_prompting(
+            profile=serialized_profile['bio'],
             paper=serialized_paper,
             model_name=self.model_name,
-            summary_prompt_template=config.prompt_template.write_review_summary,
-            strength_prompt_template=config.prompt_template.write_review_strength,
-            weakness_prompt_template=config.prompt_template.write_review_weakness,
-            score_prompt_template=config.prompt_template.write_review_score,
+            prompt_template=config.prompt_template.review_paper,
+        )[0]
+        review_score = review_score_prompting(
+            profile=serialized_profile['bio'],
+            paper_review=paper_review,
+            model_name=self.model_name,
+            prompt_template=config.prompt_template.review_score,
         )
-        return ResearchReviewForPaperSubmission(
+        return AgentPaperReviewLog(
+            timestep=(int)(datetime.now().timestamp()),
             paper_pk=paper.pk,
-            reviewer_pk=self.profile.pk,
-            summary=summary,
-            strength=strength,
-            weakness=weakness,
-            score=score,
+            agent_pk=self.profile.pk,
+            review_content=paper_review,
+            review_score=review_score,
         )
 
     @beartype
-    @chair_required
-    def write_meta_review(
-        self,
-        paper: ResearchPaperSubmission,
-        reviews: List[ResearchReviewForPaperSubmission],
-        rebuttals: List[ResearchRebuttalForPaperSubmission],
-        config: Config,
-    ) -> ResearchMetaReviewForPaperSubmission:
+    def write_paper_meta_review(
+        self, paper: ResearchPaperSubmission, reviews: List[AgentPaperReviewLog],rebuttals:AgentPaperRebuttalLog, config: Config
+    ) -> AgentPaperMetaReviewLog:
         serialized_paper = self.serializer.serialize(paper)
         serialized_reviews = self.serializer.serialize(reviews)
-        serialized_rebuttals = self.serializer.serialize(rebuttals)
+        serialized_rebuttals=self.serializer.serialize(rebuttals)
 
-        summary, strength, weakness, decision = write_meta_review_prompting(
+        meta_review = write_meta_review_prompting(
             paper=serialized_paper,
             reviews=serialized_reviews,
             rebuttals=serialized_rebuttals,
             model_name=self.model_name,
-            summary_prompt_template=config.prompt_template.write_meta_review_summary,
-            strength_prompt_template=config.prompt_template.write_meta_review_strength,
-            weakness_prompt_template=config.prompt_template.write_meta_review_weakness,
-            decision_prompt_template=config.prompt_template.write_meta_review_decision,
+            prompt_template=config.prompt_template.write_meta_review,
         )
+        review_decision = 'accept' in meta_review[0].lower()
 
-        return ResearchMetaReviewForPaperSubmission(
+        return AgentPaperMetaReviewLog(
+            timestep=(int)(datetime.now().timestamp()),
             paper_pk=paper.pk,
-            chair_pk=self.profile.pk,
-            reviewer_pks=[review.reviewer_pk for review in reviews],
-            author_pk=self.profile.pk,
-            summary=summary,
-            strength=strength,
-            weakness=weakness,
-            decision=decision,
+            agent_pk=self.profile.pk,
+            decision=review_decision,
+            meta_review=meta_review[0],
         )
 
     @beartype
-    @proj_leader_required
     def write_rebuttal(
-        self,
-        paper: ResearchPaperSubmission,
-        review: ResearchReviewForPaperSubmission,
-        config: Config,
-    ) -> ResearchRebuttalForPaperSubmission:
+        self, paper: ResearchPaperSubmission, review: AgentPaperReviewLog, config: Config
+    ) -> AgentPaperRebuttalLog:
         serialized_paper = self.serializer.serialize(paper)
         serialized_review = self.serializer.serialize(review)
 
@@ -185,9 +159,28 @@ class BaseResearchAgent(object):
             prompt_template=config.prompt_template.write_rebuttal,
         )[0]
 
-        return ResearchRebuttalForPaperSubmission(
+        return AgentPaperRebuttalLog(
+            timestep=(int)(datetime.now().timestamp()),
             paper_pk=paper.pk,
-            reviewer_pk=review.reviewer_pk,
-            author_pk=self.profile.pk,
-            content=rebuttal_content,
+            agent_pk=self.profile.pk,
+            rebuttal_content=rebuttal_content,
+        )
+
+    @beartype
+    def discuss(
+        self, message: AgentAgentDiscussionLog, config: Config
+    ) -> AgentAgentDiscussionLog:
+        serialized_message = self.serializer.serialize(message)
+        message_content = discuss_prompting(
+            message=serialized_message,
+            model_name=self.model_name,
+            prompt_template=config.prompt_template.discuss,
+        )[0]
+        return AgentAgentDiscussionLog(
+            timestep=(int)(datetime.now().timestamp()),
+            agent_from_pk=message.agent_from_pk,
+            agent_from_name=message.agent_from_name,
+            agent_to_pk=message.agent_to_pk,
+            agent_to_name=message.agent_to_name,
+            message=message_content,
         )
