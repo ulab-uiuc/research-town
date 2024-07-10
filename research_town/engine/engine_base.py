b555acb0a7358318486a19195b39a6115eb5cb7f
ft2023 <129666640+ft2023@users.noreply.github.com>
2024-07-10 22:10:20 +0800

    Add files via upload

--- research_town/engine/engine_base.py
+++ research_town/engine/engine_base.py
@@ -1,15 +1,23 @@
 from typing import Dict
-
+from beartype.typing import List,Tuple
 from ..envs.env_base import BaseMultiAgentEnv
-
-
+from ..dbs import (
+    AgentProfile,
+    PaperProfile,
+    ResearchPaperSubmission,
+    PaperProfileDB,
+    AgentProfileDB
+)
+import random
 class BaseResearchEngine:
-    def __init__(self) -> None:
+    def __init__(self,agent_db:AgentProfileDB,paper_db:PaperProfileDB) -> None:
         self.time_step = 0
         self.envs: Dict[str, BaseMultiAgentEnv] = {}
         self.transition_matrix: Dict[str, Dict[bool, str]] = {}
         self.curr_env_name: str = ''
         self.curr_env: BaseMultiAgentEnv = None
+        self.agent_db=agent_db
+        self.paper_db=paper_db
 
     def add_env(self, name: str, env: BaseMultiAgentEnv):
         self.envs[name] = env
@@ -34,17 +42,31 @@ class BaseResearchEngine:
             self.curr_env = self.envs[next_env_name]
             self.curr_env.on_enter()
 
-    def find_proj_participants(self) -> None:
-        pass
+    def find_proj_participants(self,potential_participants_profiles:List[AgentProfile]) -> List[str]:
+        leader_profile=self.leader.bio
+        self.proj_participants_pks=self.agent_db.match(query=leader_profile,agent_profiles=potential_participants_profiles)
+        return self.proj_participants_pks
 
-    def find_proj_leaders(self) -> None:
-        pass
+    def find_proj_leaders(self, leader_profile:AgentProfile) -> None:
+        self.leader=leader_profile
 
-    def find_reviewers(self) -> None:
-        pass
+    def find_reviewers(self,paper_submission:ResearchPaperSubmission,potential_reviewers_profiles:List[AgentProfile]) -> List[str]:
+        paper_abstract=paper_submission.abstract
+        self.reviewers_pks = self.agent_db.match(query=paper_abstract,
+                                                    agent_profiles=potential_reviewers_profiles)
+        return self.reviewers_pks
 
-    def find_chairs(self) -> None:
-        pass
+
+    def find_chairs(self) -> Tuple[str,List[str]]:
+        self.chair_pk = random.choice(self.reviewers_pks)
+        self.other_reviewer_pks = [reviewer for reviewer in self.reviewers_pks if reviewer != self.chair_pk]
+
+        return self.chair_pk,self.other_reviewer_pks
+
+    def read_papers(self,potential_paper_profiles:List[PaperProfile])-> List[str]:
+        leader_profile = self.leader.bio
+        paper_dks=self.paper_db.match(query=leader_profile,paper_profiles=potential_paper_profiles)
+        return paper_dks
 
     def recover(self) -> None:
         pass
