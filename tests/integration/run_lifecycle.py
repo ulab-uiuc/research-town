251a47daa92415dc47800f2c4b22dfb3b51ea0f4
Haofei Yu <1125027232@qq.com>
2024-07-13 18:42:07 -0400

    change name

--- tests/integration/run_lifecycle.py
+++ tests/integration/run_lifecycle.py
@@ -1,7 +1,7 @@
 from unittest.mock import MagicMock, patch
 
 from research_town.configs import Config
-from research_town.engines import TwoStageResearchEngine
+from research_town.engines import LifecycleResearchEngine
 from tests.mocks.mocking_func import mock_prompting
 
 from ..constants.data_constants import agent_profile_A
@@ -18,7 +18,7 @@ def test_research_lifecycle_two_stage(mock_model_prompting: MagicMock) -> None:
     mock_model_prompting.side_effect = mock_prompting
 
     example_agent_db.reset_role_avaialbility()
-    engine = TwoStageResearchEngine(
+    engine = LifecycleResearchEngine(
         agent_db=example_agent_db,
         paper_db=example_paper_db,
         progress_db=example_progress_db,
