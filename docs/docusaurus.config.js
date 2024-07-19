5191ddd603cdf83791c171acebff5d681e592296
Haofei Yu <1125027232@qq.com>
2024-07-19 14:37:03 -0400

    ci(docs): deploy doc to website (#411)
    * deploy docs workflow
    
    * update pyproject
    
    * update pyproject
    
    * modify the pages
    
    Create CNAME

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://auto-research.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -105,7 +105,7 @@ const config = {
             items: [
               {
                 label: 'Documentation',
-                to: '/docs/intro',
+                to: '/docs/about',
               },
             ],
           },
