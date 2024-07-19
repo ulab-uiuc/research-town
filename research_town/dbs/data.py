acc46e76c4dbc04a2445555ab8aa95bb0ddc3de5
Haofei Yu <1125027232@qq.com>
2024-07-19 03:36:55 -0400

    support project name

--- research_town/dbs/data.py
+++ research_town/dbs/data.py
@@ -11,6 +11,7 @@ class BaseDBData(BaseModel):
 class AgentProfile(BaseDBData):
     name: str
     bio: str
+    project_name: Optional[str] = Field(default=None)
     collaborators: Optional[List[str]] = Field(default=[])
     institute: Optional[str] = Field(default=None)
     embed: Optional[Any] = Field(default=None)
@@ -24,6 +25,7 @@ class PaperProfile(BaseDBData):
     authors: List[str] = Field(default=[])
     title: str
     abstract: str
+    project_name: Optional[str] = Field(default=None)
     url: Optional[str] = Field(default=None)
     timestamp: Optional[int] = Field(default=None)
     section_contents: Optional[Dict[str, str]] = Field(default=None)
@@ -42,6 +44,7 @@ class AgentPaperLiteratureReviewLog(BaseDBData):
     timestep: int = Field(default=0)
     paper_pks: List[str]
     agent_pk: str
+    project_name: Optional[str] = Field(default=None)
     insight_pks: Optional[List[str]] = Field(default=[])
     other_agent_pks: Optional[List[str]] = Field(default=[])
 
@@ -50,12 +53,14 @@ class AgentIdeaBrainstormingLog(BaseDBData):
     timestep: int = Field(default=0)
     idea_pk: str
     agent_pk: str
+    project_name: Optional[str] = Field(default=None)
     other_agent_pks: Optional[List[str]] = Field(default=[])
 
 
 class AgentAgentCollaborationFindingLog(BaseDBData):
     timestep: int = Field(default=0)
     agent_pk: str
+    project_name: Optional[str] = Field(default=None)
     other_agent_pks: Optional[List[str]] = Field(default=[])
 
 
@@ -65,6 +70,7 @@ class AgentAgentIdeaDiscussionLog(BaseDBData):
     agent_from_name: str
     agent_to_pk: str
     agent_to_name: str
+    project_name: Optional[str] = Field(default=None)
     message: Optional[str] = Field(default=None)
 
 
@@ -72,6 +78,7 @@ class AgentPaperWritingLog(BaseDBData):
     timestep: int = Field(default=0)
     paper_pk: str
     agent_pk: str
+    project_name: Optional[str] = Field(default=None)
     other_agent_pks: Optional[List[str]] = Field(default=[])
 
 
@@ -79,6 +86,7 @@ class AgentPaperReviewWritingLog(BaseDBData):
     timestep: int = Field(default=0)
     paper_pk: str
     agent_pk: str
+    project_name: Optional[str] = Field(default=None)
     other_agent_pks: Optional[List[str]] = Field(default=[])
     score: Optional[int] = Field(default=0)
     summary: Optional[str] = Field(default=None)
@@ -90,6 +98,7 @@ class AgentPaperRebuttalWritingLog(BaseDBData):
     timestep: int = Field(default=0)
     paper_pk: str
     agent_pk: str
+    project_name: Optional[str] = Field(default=None)
     other_agent_pks: Optional[List[str]] = Field(default=[])
     rebuttal_content: Optional[str] = Field(default=None)
 
@@ -98,6 +107,7 @@ class AgentPaperMetaReviewWritingLog(BaseDBData):
     timestep: int = Field(default=0)
     paper_pk: str
     agent_pk: str
+    project_name: Optional[str] = Field(default=None)
     other_agent_pks: Optional[List[str]] = Field(default=[])
     decision: Optional[bool] = Field(default=False)
     summary: Optional[str] = Field(default=None)
@@ -106,18 +116,21 @@ class AgentPaperMetaReviewWritingLog(BaseDBData):
 
 
 class ResearchInsight(BaseDBData):
+    project_name: Optional[str] = Field(default=None)
     content: Optional[str] = Field(default=None)
     eval_score: Optional[List[int]] = Field(default=[])  # evaluation scores
     model_config = ConfigDict(extra='allow')
 
 
 class ResearchIdea(BaseDBData):
+    project_name: Optional[str] = Field(default=None)
     content: Optional[str] = Field(default=None)
     eval_score: Optional[List[int]] = Field(default=[])  # evaluation scores
     model_config = ConfigDict(extra='allow')
 
 
 class ResearchPaperSubmission(BaseDBData):
+    project_name: Optional[str] = Field(default=None)
     abstract: str
     title: Optional[str] = Field(default=None)
     content: Optional[str] = Field(default=None)
@@ -127,6 +140,7 @@ class ResearchPaperSubmission(BaseDBData):
 
 
 class ResearchReview(BaseDBData):
+    project_name: Optional[str] = Field(default=None)
     paper_pk: Optional[str] = Field(default=None)
     reviewer_pk: Optional[str] = Field(default=None)
     summary: Optional[str] = Field(default=None)
@@ -138,6 +152,7 @@ class ResearchReview(BaseDBData):
 
 
 class ResearchRebuttal(BaseDBData):
+    project_name: Optional[str] = Field(default=None)
     paper_pk: Optional[str] = Field(default=None)
     reviewer_pk: Optional[str] = Field(default=None)
     author_pk: Optional[str] = Field(default=None)
@@ -147,6 +162,7 @@ class ResearchRebuttal(BaseDBData):
 
 
 class ResearchMetaReview(BaseDBData):
+    project_name: Optional[str] = Field(default=None)
     paper_pk: Optional[str] = Field(default=None)
     chair_pk: Optional[str] = Field(default=None)
     reviewer_pks: List[str] = Field(default=[])
