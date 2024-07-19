d374fede44be01995fc71384a11c2feea589a31a
lwaekfjlk <1125027232@qq.com>
2024-07-19 14:35:36 -0400

    modify the pages

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
             items: [
               {
                 label: 'Documentation',
-                to: '/docs/intro',
+                to: '/docs/about',
               },
             ],
           },
