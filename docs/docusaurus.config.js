3074021e42a0cf222273ff3de8bd380419b860a3
Haofei Yu <1125027232@qq.com>
2024-07-19 17:31:11 -0400

    bug(doc): fix website deploy (#415)
    * update cname
    
    * update
    
    * update url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://auto-research.dev',
+  url: 'https://docs.auto-research.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
