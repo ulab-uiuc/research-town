f2cc5e4c576dcff3b02fae48ec7d23612c628f0c
Haofei Yu <1125027232@qq.com>
2024-07-19 14:37:03 -0400

    ci(docs): deploy doc to website (#411)
    * deploy docs workflow
    
    * update pyproject
    
    * update pyproject
    
    * modify the pages
    
    Create CNAME

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/about">
             Get started - 5min ⏱️
           </Link>
         </div>
