693d6b181d51fcd423f22e79470f1a94e0a1b128
Haofei Yu <1125027232@qq.com>
2024-09-10 23:16:09 -0500

    fix pre-commit errror

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,6 @@
-import gradio as gr
 from typing import List
+
+import gradio as gr
 
 
 def get_proposals() -> List[str]:
@@ -10,7 +11,7 @@ with gr.Blocks() as demo:
     input_text = gr.Textbox(label='input')
     mode = gr.Radio(['textbox', 'button'], value='textbox')
 
-    @gr.render(inputs=[input_text, mode], triggers=[input_text.submit]) # type: ignore
+    @gr.render(inputs=[input_text, mode], triggers=[input_text.submit])  # type: ignore
     def show_ideas(text, mode):
         if len(text) == 0:
             gr.Markdown('## No arxiv link provided')
