c918acaafd0a421e22d6edd02d0ec486e009c60d
Haofei Yu <1125027232@qq.com>
2024-09-10 23:21:00 -0500

    feat(frontend): support init frontend (#559)
    * set lock
    
    * fix pre-commit error
    
    * change idea to proposal
    
    * fix mypy errror
    
    * fix pre-commit errror
    
    * update poetry lock

---
+++ frontend/app.py
@@ -0,0 +1,27 @@
+from typing import List
+
+import gradio as gr
+
+
+def get_proposals() -> List[str]:
+    return ['idea 1', 'idea 2', 'idea 3']
+
+
+with gr.Blocks() as demo:
+    input_text = gr.Textbox(label='input')
+    mode = gr.Radio(['textbox', 'button'], value='textbox')
+
+    @gr.render(inputs=[input_text, mode], triggers=[input_text.submit])  # type: ignore
+    def show_ideas(text, mode):
+        if len(text) == 0:
+            gr.Markdown('## No arxiv link provided')
+        else:
+            proposals = get_proposals()
+            for proposal in proposals:
+                if mode == 'textbox':
+                    gr.Textbox(proposal)
+                else:
+                    gr.Button(proposal)
+
+
+demo.launch()
