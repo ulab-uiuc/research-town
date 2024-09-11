471d2b760d14932915073228a4252692617bb1a6
Haofei Yu <1125027232@qq.com>
2024-09-11 10:36:00 -0500

    feat(frontend): support full frontend (#568)
    * support full frontend
    
    * fix ci errors
    
    * fix ci errors
    
    * fix ci errors
    
    * fix ci errors

--- frontend/app.py
+++ frontend/app.py
@@ -1,27 +1,80 @@
-from typing import List
+import os
+from typing import List, Optional
 
 import gradio as gr
 
+from research_town.configs import Config
+from research_town.dbs import LogDB, PaperDB, ProgressDB, Proposal, ResearcherDB
+from research_town.engines import Engine
+from research_town.utils.paper_collector import get_paper_content
 
-def get_proposals() -> List[str]:
-    return ['idea 1', 'idea 2', 'idea 3']
+
+def get_proposals(introduction: str) -> List[str]:
+    config_file_path = '../configs'
+    save_file_path = '../examples/research_town_demo_log'
+    agent_names = [
+        'Jiaxuan You',
+        'Jure Leskovec',
+        'Stefanie Jegelka',
+        'Silvio Lattanzi',
+        'Rex Ying',
+        'Tim Althoff',
+        'Christos Faloutsos',
+        'Julian McAuley',
+    ]
+    # if save path exists, then load
+    config = Config(config_file_path)
+    agent_db = ResearcherDB()
+    paper_db = PaperDB()
+    if os.path.exists(save_file_path):
+        agent_db.load_from_json(save_file_path, with_embed=True)
+        paper_db.load_from_json(save_file_path, with_embed=True)
+    else:
+        agent_db.pull_agents(agent_names=agent_names, config=config)
+        paper_db.pull_papers(num=10, domain='graph neural networks')
+
+    env_db = LogDB()
+    progress_db = ProgressDB()
+    engine = Engine(
+        project_name='research_town_demo',
+        agent_db=agent_db,
+        paper_db=paper_db,
+        progress_db=progress_db,
+        env_db=env_db,
+        config=config,
+    )
+    engine.run(task=introduction)
+    proposals = progress_db.get(Proposal)
+    contents = [proposal.abstract for proposal in proposals]
+    return contents
+
+
+def get_introduction(url: str) -> Optional[str]:
+    contents = get_paper_content(url)
+    if contents is None or contents[0] is None:
+        return None
+    section_contents = contents[0]
+    for section_name, section_content in section_contents.items():
+        if 'Introduction' in section_name:
+            return section_content
+    return None
 
 
 with gr.Blocks() as demo:
-    input_text = gr.Textbox(label='input')
-    mode = gr.Radio(['textbox', 'button'], value='textbox')
+    input_url = gr.Textbox(label='input')
 
-    @gr.render(inputs=[input_text, mode], triggers=[input_text.submit])  # type: ignore
-    def show_ideas(text, mode):
-        if len(text) == 0:
-            gr.Markdown('## No arxiv link provided')
+    @gr.render(inputs=[input_url], triggers=[input_url.submit])  # type: ignore
+    def show_proposals(url):
+        if 'https://www.arxiv.org' not in url:
+            gr.Markdown('We only accept arxiv links.')
         else:
-            proposals = get_proposals()
-            for proposal in proposals:
-                if mode == 'textbox':
+            introduction = get_introduction(url)
+            if introduction is None:
+                gr.Markdown('Paper parsing error.')
+            else:
+                proposals = get_proposals(introduction)
+                for proposal in proposals:
                     gr.Textbox(proposal)
-                else:
-                    gr.Button(proposal)
 
 
 demo.launch()
--- research_town/dbs/db_base.py
+++ research_town/dbs/db_base.py
@@ -47,8 +47,8 @@ class BaseDB(Generic[T]):
             return True
         return False
 
-    def get(self, **conditions: Union[str, int, float, List[int]]) -> List[T]:
-        if conditions == {}:
+    def get(self, **conditions: Union[str, int, float, List[int], None]) -> List[T]:
+        if conditions == {} or conditions is None:
             return list(self.data.values())
         result = []
         for data in self.data.values():
