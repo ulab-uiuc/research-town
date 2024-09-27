8c42f5acc03c1c44d580e4a682776c73647d817e
chengzr01 <ziruicheng01@gmail.com>
2024-09-27 00:27:02 -0500

    update agent domain

--- frontend/src/components/ProgressDisplay/IdeaDisplay.jsx
+++ frontend/src/components/ProgressDisplay/IdeaDisplay.jsx
@@ -5,31 +5,32 @@ import ReactMarkdown from "react-markdown";
 import Accordion from "react-bootstrap/Accordion";
 
 function IdeaDisplay({ list }) {
-    return (
-        <>
-        <Row className="mb-3">
-            <Col>
-            <h2>Ideas</h2>
-            </Col>
-        </Row>
-        <Row>
-            <Accordion>
-            {list.map((item, index) => (
-                <Accordion.Item key={index} eventKey="0">
-                <Accordion.Header>
-                    #{index + 1} {item.type} from {item.agent_name}
-                </Accordion.Header>
-                <Accordion.Body style={{ textAlign: "left" }}>
-                    <p>
-                    <ReactMarkdown>{item.content}</ReactMarkdown>
-                    </p>
-                </Accordion.Body>
-                </Accordion.Item>
-            ))}
-            </Accordion>
-        </Row>
-        </>
-    );
+  return (
+    <>
+      <Row className="mb-3">
+        <Col>
+          <h2>Ideas</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Accordion>
+          {list.map((item, index) => (
+            <Accordion.Item key={index} eventKey="0">
+              <Accordion.Header>
+                {item.type.charAt(0).toUpperCase() + item.type.slice(1)} #
+                {index + 1} from {item.agent_domain} Expert Agent
+              </Accordion.Header>
+              <Accordion.Body style={{ textAlign: "left" }}>
+                <p>
+                  <ReactMarkdown>{item.content}</ReactMarkdown>
+                </p>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      </Row>
+    </>
+  );
 }
 
 export default IdeaDisplay;
--- frontend/src/components/ProgressDisplay/InsightDisplay.jsx
+++ frontend/src/components/ProgressDisplay/InsightDisplay.jsx
@@ -5,29 +5,29 @@ import Col from "react-bootstrap/Col";
 import ExpandableCard from "./ExpandableCard";
 
 function InsightDisplay({ list }) {
-    return (
-        <>
-        <Row className="mb-3">
-            <Col>
-            <h2>Insights</h2>
-            </Col>
-        </Row>
-        <Row>
-            {list.map((insight, index) => (
-            <Col xs={4}>
-                <ExpandableCard
-                key={index}
-                index={index + 1}
-                type={insight.type}
-                summary={""}
-                content={insight.content}
-                name={insight.agent_name}
-                />
-            </Col>
-            ))}
-        </Row>
-        </>
-    );
+  return (
+    <>
+      <Row className="mb-3">
+        <Col>
+          <h2>Insights</h2>
+        </Col>
+      </Row>
+      <Row>
+        {list.map((insight, index) => (
+          <Col xs={4}>
+            <ExpandableCard
+              key={index}
+              index={index + 1}
+              type={insight.type}
+              summary={""}
+              content={insight.content}
+              name={insight.agent_domain + " Expert Agent"}
+            />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
 
 export default InsightDisplay;
