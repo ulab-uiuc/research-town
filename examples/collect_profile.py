ba6b73c29e748015074d721bed6084be9eb7d481
Haofei Yu <1125027232@qq.com>
2024-09-26 23:17:00 -0500

    fix mypy

--- examples/collect_profile.py
+++ examples/collect_profile.py
@@ -4,7 +4,7 @@ from research_town.configs import Config
 from research_town.dbs import ProfileDB
 
 
-def collect_profile(names: List[str], config: Config, save_path: str):
+def collect_profile(names: List[str], config: Config, save_path: str) -> None:
     db = ProfileDB()
     for name in names:
         db.pull_profiles(names=[name], config=config)
