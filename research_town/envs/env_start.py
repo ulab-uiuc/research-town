6f2c3658545900a89c10660fdf4fec0e66fc023e
Haofei Yu <1125027232@qq.com>
2024-09-30 02:03:56 -0500

    find leader not sample

--- research_town/envs/env_start.py
+++ research_town/envs/env_start.py
@@ -29,7 +29,7 @@ class StartEnv(BaseEnv):
         contexts_to_text = ''
         for idx, context in enumerate(self.contexts):
             contexts_to_text += f'Paper {idx + 1}: \n\n{context}\n\n'
-        self.leader = self.agent_manager.sample_leader()
+        self.leader = self.agent_manager.find_leader(task=contexts_to_text)
 
     def run(self) -> Generator[Tuple[Progress, Agent], None, None]:
         if False:
