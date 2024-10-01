e83ee9890a9dd3e9781feb3be86be6136833c208
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2024-09-30 22:06:21 -0500

    Update run_evaluation.sh

--- research_bench/run_evaluation.sh
+++ research_bench/run_evaluation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 INPUT="./benchmark/benchmark_0930.json"
-OUTPUT="./results/research_bench_result.jsonl"
+OUTPUT="./results/research_bench_result_v1.jsonl"
 
 poetry run python run_evaluation.py --input "$INPUT" --output "$OUTPUT"
