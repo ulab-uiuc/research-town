afc6d1e8e6480f42c94153f14084a3fb4af166ca
Haofei Yu <1125027232@qq.com>
2024-10-22 18:53:30 -0500

    move and change directory

--- research_bench/run_eval.sh
+++ research_bench/run_eval.sh
@@ -4,4 +4,4 @@ INPUT_PATH="./benchmark/crossbench.json"
 MODE="citation_only"
 OUTPUT_PATH="./results/crossbench_result_4o_mini_${MODE}.jsonl"
 
-poetry run python run_evaluation.py --input "$INPUT_PATH" --output "$OUTPUT_PATH" --mode "$MODE"
+poetry run python run_eval.py --input "$INPUT_PATH" --output "$OUTPUT_PATH" --mode "$MODE"
