67cbf86b34d23d36cadc4d4b46ee74f46076c290
Haofei Yu <1125027232@qq.com>
2024-10-23 11:10:01 -0500

    fix pytest error

--- research_bench/collect_nips_paper_links.py
+++ research_bench/collect_nips_paper_links.py
@@ -24,6 +24,18 @@ def save_paper_urls(titles: List[str], output_file: str) -> None:
                 f.write(f'{url}\n')
 
 
+def save_latest_paper_urls(
+    output_file_full: str, output_file_latest: str, paper_num: int
+) -> None:
+    with open(output_file_full, 'r') as f:
+        lines = f.readlines()
+    lines.sort()
+    lines = lines[-paper_num:]
+    with open(output_file_latest, 'w') as f:
+        for line in lines:
+            f.write(line)
+
+
 def main() -> None:
     parser = argparse.ArgumentParser(description='Scrape and save NeurIPS paper URLs.')
     parser.add_argument(
@@ -33,19 +45,32 @@ def main() -> None:
         help='URL of the NeurIPS paper list',
     )
     parser.add_argument(
-        '--output',
+        '--output_full',
         type=str,
         default='./mlbench/mlbench_paper_links.txt',
         help='Output file to save the paper URLs',
+    )
+    parser.add_argument(
+        '--output_latest',
+        type=str,
+        default='./mlbench/mlbench_paper_links_latest.txt',
+        help='Output file to save the paper URLs',
+    )
+    parser.add_argument(
+        '--latest_paper_num',
+        type=int,
+        default=100,
+        help='Number of latest papers to save',
     )
 
     args = parser.parse_args()
     print(f'Fetching paper titles from {args.url}...')
     titles = fetch_paper_titles(args.url)
 
-    print(f'Saving paper URLs to {args.output}...')
-    save_paper_urls(titles, args.output)
+    print(f'Saving paper URLs to {args.output_full}...')
+    save_paper_urls(titles, args.output_full)
 
+    save_latest_paper_urls(args.output_full, args.output_latest, args.latest_paper_num)
     print('Done!')
 
 
--- research_bench/proposal_writing.py
+++ research_bench/proposal_writing.py
@@ -164,7 +164,7 @@ def write_proposal_with_profiles_and_citations(
     return response
 
 
-def write_predicted_proposal(
+def write_proposal(
     mode: str,
     profiles: List[Profile],
     ref_contents: List[str],
@@ -186,36 +186,3 @@ def write_predicted_proposal(
         )
     else:
         raise ValueError(f'Invalid proposal writing mode: {mode}')
-
-
-def write_reference_proposal(intro: str, config: Config) -> str:
-    prompt = [
-        {
-            'role': 'user',
-            'content': (
-                'Here is a high-level summarized insight of a research field Machine Learning.\n\n'
-                'Here are the five core questions:\n\n'
-                '[Question 1] - What is the problem?\n\n'
-                'Formulate the specific research question you aim to address. Only output one question and do not include any more information.\n\n'
-                '[Question 2] - Why is it interesting and important?\n\n'
-                'Explain the broader implications of solving this problem for the research community.\n'
-                'Discuss how such paper will affect the future research.\n'
-                'Discuss how addressing this question could advance knowledge or lead to practical applications.\n\n'
-                '[Question 3] - Why is it hard?\n\n'
-                'Discuss the challenges and complexities involved in solving this problem.\n'
-                'Explain why naive or straightforward approaches may fail.\n'
-                'Identify any technical, theoretical, or practical obstacles that need to be overcome. MAKE IT CLEAR.\n\n'
-                "[Question 4] - Why hasn't it been solved before?\n\n"
-                'Identify gaps or limitations in previous research or existing solutions.\n'
-                'Discuss any barriers that have prevented this problem from being solved until now.\n'
-                'Explain how your approach differs from or improves upon prior work. MAKE IT CLEAR.\n\n'
-                '[Question 5] - What are the key components of my approach and results?\n\n'
-                'Outline your proposed methodology in detail, including the method, dataset, metric that you plan to use.\n'
-                'Describe the expected outcomes. MAKE IT CLEAR.\n\n'
-                f'Introduction:\n{intro}\n\n'
-                'Please provide the five core questions contents based on the above introduction.'
-            ),
-        }
-    ]
-    response = model_prompting(config.param.base_llm, prompt)[0]
-    return response
