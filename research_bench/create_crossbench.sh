e9385e86eb6389e35fe98f456e6f28773cdecd64
Haofei Yu <1125027232@qq.com>
2024-10-23 10:39:20 -0500

    move txt file position

--- research_bench/create_crossbench.sh
+++ research_bench/create_crossbench.sh
@@ -1,1 +1,1 @@
-python crossbench.py --input ./crossbench_paper_links.txt --output ./crossbench/crossbench.json
+python crossbench.py --input ./crossbench/crossbench_paper_links.txt --output ./crossbench/crossbench.json
--- research_bench/create_mlbench.sh
+++ research_bench/create_mlbench.sh
@@ -1,1 +1,1 @@
-python crossbench.py --input ./mlbench_paper_links.txt --output ./mlbench/crossbench.json
+python crossbench.py --input ./mlbench/mlbench_paper_links.txt --output ./mlbench/mlbench.json
