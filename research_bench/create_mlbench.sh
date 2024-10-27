7135dc78cdad93f8b0d677e0aa8007598552e5d5
Haofei Yu <1125027232@qq.com>
2024-10-26 22:59:49 -0500

    uncomment lines

--- research_bench/create_mlbench.sh
+++ research_bench/create_mlbench.sh
@@ -1,1 +1,1 @@
-python create_bench_from_paper_links.py --input ./mlbench/mlbench_paper_links_latest.txt --output ./mlbench/mlbench.json
+python create_bench_from_paper_links.py --input ./mlbench/mlbench_paper_links.txt --output ./mlbench/mlbench.json
