d4680e66bf8c8f66d7788e283f11a5942003962f
Haofei Yu <1125027232@qq.com>
2024-10-26 23:34:27 -0500

    simplify functions in extension

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,31 +1,10 @@
-from research_town.configs import Config, DatabaseConfig
+from research_town.configs import Config
 from research_town.dbs import LogDB, PaperDB, ProfileDB, ProgressDB
-
-
-def get_profile_db(database_config: DatabaseConfig) -> ProfileDB:
-    db = ProfileDB(config=database_config)
-    return db
-
-
-def get_paper_db(database_config: DatabaseConfig) -> PaperDB:
-    db = PaperDB(config=database_config)
-    return db
-
-
-def get_log_db(database_config: DatabaseConfig) -> LogDB:
-    db = LogDB(config=database_config)
-    return db
-
-
-def get_progress_db(database_config: DatabaseConfig) -> ProgressDB:
-    db = ProgressDB(config=database_config)
-    return db
-
 
 config_file_path = '../configs'
 config = Config(config_file_path)
 
-profile_db = get_profile_db(config.database)
-paper_db = get_paper_db(config.database)
-log_db = get_log_db(config.database)
-progress_db = get_progress_db(config.database)
+profile_db = ProfileDB(config.database)
+paper_db = PaperDB(config.database)
+log_db = LogDB(config.database)
+progress_db = ProgressDB(config.database)
