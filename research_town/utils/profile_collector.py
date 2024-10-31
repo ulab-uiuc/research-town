76952873eebcff409913eb3002bb331ffb9eb83d
keyangds <107345948+keyangds@users.noreply.github.com>
2024-10-30 21:05:40 -0500

    using sematic scholar to pull profile

--- research_town/utils/profile_collector.py
+++ research_town/utils/profile_collector.py
@@ -1,7 +1,6 @@
-from arxiv import Client, Search
 from beartype import beartype
 from beartype.typing import Dict, List, Optional, Tuple, Union
-from tqdm import tqdm
+from semanticscholar import SemanticScholar
 
 from .error_handler import api_calling_error_exponential_backoff
 from .model_prompting import model_prompting
@@ -30,22 +29,43 @@ def coauthor_filter(co_authors: Dict[str, int], limit: int = 5) -> List[str]:
 def collect_publications_and_coauthors(
     author: str, paper_max_num: int = 10, exclude_paper_titles: List[str] = []
 ) -> Tuple[List[str], List[str], List[str]]:
-    client = Client()
+    semantic_client = SemanticScholar()
+
+    # Retrieve author ID from Semantic Scholar
+    author_id = None
+    search_results = semantic_client.search_author(author, fields=['papers'])
+    for result in search_results:
+        for paper in result['papers']:
+            if paper['title'].lower() in [
+                title.lower() for title in exclude_paper_titles
+            ]:
+                author_id = result['authorId']
+                break
+        if author_id:
+            break
+
+    if not author_id:
+        print('Author not found or paper name does not match any records.')
+        return [], [], []
+
     paper_abstracts = []
     paper_titles = []
     co_authors: Dict[str, int] = {}
-    search = Search(query=f'au:{author}', max_results=paper_max_num)
-    for result in tqdm(
-        client.results(search), desc=f"Collecting {author}'s papers", unit='Paper'
-    ):
-        if result.title in exclude_paper_titles:
+    author_data = semantic_client.get_author(
+        author_id, fields=['papers.authors', 'papers.title', 'papers.abstract']
+    )
+    papers = author_data['papers'][:paper_max_num]
+    for paper in papers:
+        if paper['title'] in exclude_paper_titles:
             continue
-        if author not in ', '.join(author.name for author in result.authors):
+
+        paper_authors = [author['name'] for author in paper['authors']]
+        if author not in ', '.join(author for author in paper_authors):
             continue
-        paper_authors = [author.name for author in result.authors]
+
         co_authors = coauthor_frequency(author, paper_authors, co_authors)
-        paper_abstract = result.summary.replace('\n', ' ')
-        paper_title = result.title
+        paper_abstract = paper['abstract'].replace('\n', ' ')
+        paper_title = paper['title']
         paper_abstracts.append(paper_abstract)
         paper_titles.append(paper_title)
     co_author_names = coauthor_filter(co_authors, limit=10)
