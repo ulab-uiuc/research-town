1b94c1c4161a5b838d60bb1fad9c253b64e76d58
Kunlun Zhu <kunlunz2@illinois.edu>
2024-11-03 01:16:02 +0000

    fix mode choices bug

--- research_bench/run_eval.py
+++ research_bench/run_eval.py
@@ -66,7 +66,7 @@ def main() -> None:
         '--mode',
         type=str,
         required=True,
-        choices=['author_only', 'citation_only', 'author_citation', 'textgnn'],
+        choices=['author_only', 'citation_only', 'author_citation', 'textgnn', 'sakana_ai_scientist'],
         help='Processing mode',
     )
     parser.add_argument(
