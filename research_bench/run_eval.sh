80ff23e1cd69e2f50d2471bfc47b9b40ef0c71aa
Keyang Xuan <107345948+keyangds@users.noreply.github.com>
2024-11-06 07:00:10 -0600

    feat(bench): support sim-based individual 5q eval (#841)
    * minor fix on mlbench
    
    * iclr pipeline
    
    * iclr_pipeline
    
    * TYPE FIX
    
    * minor fix
    
    * modify iclr paper_link collection to general conference paper linke collection
    
    * creating bench creation for conference
    
    * minor fix
    
    * using sematic scholar to pull profile
    
    * record progress
    
    * minor fix
    
    * update pyproject
    
    * update poetry lock
    
    * error handling
    
    * add individual question embedding similarity eval
    
    * type fix
    
    * type fix
    
    ---------
    
    Co-authored-by: Haofei Yu <1125027232@qq.com>

--- research_bench/run_eval.sh
+++ research_bench/run_eval.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Define the input and output paths, along with the modes to test
-INPUT_PATH="./mlbench/mlbench.json"
+INPUT_PATH="./benchmark/mlbench.json"
 OUTPUT_DIR="./results"
 MODES=("sakana_ai_scientist") #("citation_only" "author_only" "author_citation")
-#MODES=("citation_only" "author_only" "author_citation")
 
 # Loop through each mode and run the evaluation
 for MODE in "${MODES[@]}"
